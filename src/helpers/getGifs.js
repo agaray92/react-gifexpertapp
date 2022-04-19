64963511a3f2f1eaf6be6ea5cae11e642943aea3
Alexander Garay Pardo <agaray@softone.com.co>
2022-04-19 09:57:40 -0400

    Aumentar numero gifs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 
     export const getGif = async ( category ) => {
-        const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=3&api_key=7lSkwUiyElTu9iZqP4clFmXsEdM9qnbQ`
+        const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=7lSkwUiyElTu9iZqP4clFmXsEdM9qnbQ`
         const respuesta = await fetch( url );
         const { data } = await respuesta.json();
         const gifs = data.map( img => {
